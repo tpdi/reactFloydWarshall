ece7b70f2039d5c54ac82426c65c4c795657d09c
TP Diffenbach <tpd@diffenbach.org>
2016-06-15 20:27:10 -0700

    Skip presentational components, just lay out everything in the container. onChnage callback working.

--- src/containers/FloydWarshall.js
+++ src/containers/FloydWarshall.js
@@ -3,11 +3,33 @@ import {RTable} from 'components';
 
 export default class FloydWarshall extends Component {
 
+  handleUpdate = (row) => (column) => (event) => {
+    alert(`row ${row} col ${column} val ${event.target.value}`);
+    return true;
+  }
+
   render() {
-    const rows = [['---1---', 2], [3, 4]];
+    const rows = [[0, 2, 1], [3, 0, 3], [5, 5, 0]];
     return (
       <div>
         <RTable rows={rows}/>
+        <table><tbody>
+          {rows.map( (row, rindex) => {
+            const rowHandler = this.handleUpdate(rindex);
+            return (
+              <tr key={rindex}>
+                { row.map( (col, cindex) => {
+                  const colHandler = rowHandler(cindex);
+                  return (
+                    <td>
+                      <input key={cindex} defaultValue={col} onBlur={colHandler} readOnly={rindex === cindex}/>
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody></table>
       </div>
     );
   }
